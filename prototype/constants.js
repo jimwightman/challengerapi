53aa93b8095ebef95efb1efcbdc2413069dcbddd
Jim Wightman <jim.wightman@outlook.com>
2013-04-29 15:53:32 -0700

    Initial Constants

---
+++ prototype/constants.js
@@ -0,0 +1,29 @@
+// constants.js, part of Challenger project
+
+var _X 			= 1; 	// X coordinate
+var _Y 			= 2;	// Y coordinate
+var _Z 			= 4; 	// Z coordinate
+var _ViewAngle 	= 8;	// ViewAngle (radians)
+var _Velocity 	= 16;	// Velocity value
+var _Mass 		= 32;	// Mass value
+var _FastArray 	= 128;	// Dump for 'other' types of value
+
+function GetPlayerPosition(Id, properyBitmask)
+{
+	//if (properyBitmask & _X)
+	//{
+		alert('X coordinate requested');
+	//}
+}
+
+
+GetPlayerPosition(0, _X);
+
+
+
+
+
+
+
+
+
